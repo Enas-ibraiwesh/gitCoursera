7464ee035cf9e42af980d979bb333941296f094d
Enas-ibraiwesh <enas.ibraiwesh@htu.edu.jo>
2023-02-14 00:53:31 +0200

    dialog

--- week 5/Angular/conFusion/src/app/app.module.ts
+++ week 5/Angular/conFusion/src/app/app.module.ts
@@ -24,6 +24,10 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { LoginComponent } from './login/login.component';
+
+import { MatDialogModule } from '@angular/material/dialog';
+
 
 @NgModule({
   declarations: [
@@ -34,7 +38,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     FooterComponent,
     HomeComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -45,6 +50,7 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     MatCardModule,
     MatButtonModule,
     FlexLayoutModule,
+    MatDialogModule,
     AppRoutingModule
 
   ],
@@ -52,6 +58,9 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     DishService,
   PromotionService,
 LeaderService],
+entryComponents: [
+  LoginComponent
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
