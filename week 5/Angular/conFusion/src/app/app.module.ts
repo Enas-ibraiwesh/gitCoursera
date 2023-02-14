713db30992704d2837e5e8d256817307b63ef5a1
Enas-ibraiwesh <enas.ibraiwesh@htu.edu.jo>
2023-02-14 20:55:53 +0200

    reactive form 1

--- week 5/Angular/conFusion/src/app/app.module.ts
+++ week 5/Angular/conFusion/src/app/app.module.ts
@@ -31,7 +31,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { FormsModule } from '@angular/forms'; 
-
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -59,8 +61,11 @@ import { FormsModule } from '@angular/forms';
     MatInputModule,
     MatCheckboxModule,
     MatDialogModule,
+    MatSelectModule,
+    MatSlideToggleModule,
     AppRoutingModule,
-    FormsModule 
+    FormsModule,
+    ReactiveFormsModule
 
 
   ],
