47abdef52f29b96aba1d8a56fa01acde0d3f9e1b
Enas-ibraiwesh <enas.ibraiwesh@htu.edu.jo>
2023-02-16 20:36:02 +0200

    rxjs 1

--- week 5/Angular/conFusion/src/app/about/about.component.ts
+++ week 5/Angular/conFusion/src/app/about/about.component.ts
@@ -16,7 +16,7 @@ export class AboutComponent implements OnInit {
 
   ngOnInit() {
    this.laederService.getLeaders()
-   .then((leaders=> this.leaders = leaders));
+   .subscribe((leaders=> this.leaders = leaders));
 
   }
 
