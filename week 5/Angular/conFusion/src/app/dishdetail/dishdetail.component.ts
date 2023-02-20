4a34883c259c04ecd806b3306524e8b8b79db634
Enas-ibraiwesh <enas.ibraiwesh@htu.edu.jo>
2023-02-20 17:13:43 +0200

    animation part 2

--- week 5/Angular/conFusion/src/app/dishdetail/dishdetail.component.ts
+++ week 5/Angular/conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -11,7 +11,7 @@ import { switchMap } from 'rxjs/operators';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 
-import { trigger, state, style, animate, transition } from '@angular/animations';
+import { visibility, flyInOut,expand } from '../animations/app.animation';
 
 
 import { Comment } from '../shared/comment';
@@ -19,18 +19,15 @@ import { Comment } from '../shared/comment';
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.scss'],
+  host: {
+    '[@flyInOut]': 'true',
+    'style': 'display: block;'
+    },
   animations: [
-    trigger('visibility', [
-        state('shown', style({
-            transform: 'scale(1.0)',
-            opacity: 1
-        })),
-        state('hidden', style({
-            transform: 'scale(0.5)',
-            opacity: 0
-        })),
-        transition('* => *', animate('0.5s ease-in-out'))
-    ])
+    flyInOut(),
+    expand(),
+    visibility()
+
   ]
 })
 export class DishdetailComponent implements OnInit {
